eb220f7458bc066f95bfb34fc474b811a1156fb5
Mix <32300164+mnixry@users.noreply.github.com>
2021-02-21 12:36:50 +0800

    Update http.js

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'https://hibiapi.herokuapp.com/api/pixiv/'
+const baseURL = 'https://api.obfs.dev/api/pixiv/'
 
 axios.defaults.baseURL = baseURL
 axios.defaults.timeout = 10000
